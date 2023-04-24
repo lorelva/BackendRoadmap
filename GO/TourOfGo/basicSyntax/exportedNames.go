935b4a21b3b04479bf2e67f6aed7afbab17375dc
LORENA VALLE <lore1268valle@gmail.com>
2023-04-24 14:39:33 -0600

    Repitela

---
+++ GO/TourOfGo/basicSyntax/exportedNames.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// use Pi instead of pi
+func main() {
+	fmt.Println(math.Pi)
+}
---
+++ GO/TourOfGo/exportedNames.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// use Pi instead of pi
+func main() {
+	var int
+	fmt.Println(math.Pi)
+}
