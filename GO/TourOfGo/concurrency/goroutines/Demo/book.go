bc34b80c36f41e775ea1039cc012d0f4fae470e5
lorelval <lore1268valle@gmail.com>
2023-07-09 18:55:11 -0600

    UNderstanding goroutines...

---
+++ GO/TourOfGo/concurrency/goroutines/Demo/book.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+type Book struct {
+	ID            int
+	Title         string
+	Author        string
+	YearPublished int
+}
+
+func (b Book) String() string {
+	return fmt.Sprintf(
+		"Title:\t\t%q\n"+
+			"Author:\t\t%q\n"+
+			"Published:\t%v\n", b.Title, b.Author, b.YearPublished)
+
+}
+
+var books = []Book{
+
+	Book{
+		ID:            1,
+		Title:         "A Soul of Ash and Blood",
+		Author:        "Jennifer L. Armentrout",
+		YearPublished: 2019,
+	},
+
+	Book{
+		ID:            2,
+		Title:         "The Perks of Being a Wallflower",
+		Author:        "Stephen Chbosky",
+		YearPublished: 2012,
+	},
+	Book{
+		ID:            3,
+		Title:         "Paper Towns",
+		Author:        "John Green",
+		YearPublished: 2015,
+	},
+
+	Book{
+		ID:            4,
+		Title:         "The Trial",
+		Author:        "Franz Kafka",
+		YearPublished: 1925,
+	},
+
+	Book{
+		ID:            5,
+		Title:         "Atomic Habits",
+		Author:        "James Clear",
+		YearPublished: 2018,
+	},
+}
