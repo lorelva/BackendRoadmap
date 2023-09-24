1d33d7e8becd19e64d35f7f5db8432e2c83b3122
lorelval <lore1268valle@gmail.com>
2023-09-24 17:47:45 -0600

    Making evaluations in special Cards

--- GO/exercises/additional/specialCards/main.go
+++ GO/exercises/additional/specialCards/main.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	k := []int{2, 3, 1, 3}
+	k := []int{3, 4, 5, 1, 1, 2, 1} //=14
 	//dont sum the numbers that are repeated so...
-	//special cards 2 , 1 --> 2+1 = 3
 	result := solve(k)
 	fmt.Println(result)
 }
@@ -18,8 +17,6 @@ func solve(k []int) int {
 	//iterar los numeros y si se encuentra repetido y en value se van guardando
 	for _, value := range k {
 		find[value]++
-		//fmt.Println(value)
-		//sum += k[value]
 	}
 	for _, value := range k {
 		//1 para saber que es un valor unico y se deberá sumar
