6d1d4a7cf512a11b98f604e8392ad1bb9f9eb42b
lorelval <lore1268valle@gmail.com>
2023-10-14 21:31:43 -0600

    Add a splitAfter to divide seconds from format of AM/PM

--- GO/exercises/hankerRank/timeConverter/main.go
+++ GO/exercises/hankerRank/timeConverter/main.go
@@ -2,22 +2,38 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-
-	s := "07:05:45PM"
+	//FORMAT HHMMSS12h = "03:04:05PM"
+	//TO 24h OutputString := "15:04:05"
+	s := "03:04:05PM"
 	conversion := timeConversion(s)
 	fmt.Println(conversion)
-
 }
-
 func timeConversion(s string) string {
+	separateTime := strings.Split(s, ":")
+	//convert to 3 slices 03:04:05PM
+	hours := separateTime[0]
+	minutes := separateTime[1]
+	seconds := separateTime[2]
+	//separate AM OR PM from seconds[2]
+	format := s[len(s)-2:]
+	fmt.Println(hours)
+	fmt.Println(minutes)
+	fmt.Println(seconds)
+	fmt.Println(format)
 
-	//FORMAT not defined in the time package : HHMMSS12h = "3:04:05 PM"
-	stringtoConvert := s
-	outputString := "19:05:45"
+	var toHour int
+	//validate if format  it's AM OR PM
+	if format == "AM" && hours == "12" {
+		hours = "00"
+		//toHour, _ = strconv.Atoi(hours)
 
+	}
+	if format == "PM " && hours != "12" {
+		hours += "12"
+	}
 	return s
-
 }
