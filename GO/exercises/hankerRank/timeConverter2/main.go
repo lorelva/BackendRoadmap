e35241edcb38e5afe20b185a7b81c91294dc5713
lorelval <lore1268valle@gmail.com>
2023-10-19 15:18:13 -0600

    Time Converter it's done ...Finally

--- GO/exercises/hankerRank/timeConverter2/main.go
+++ GO/exercises/hankerRank/timeConverter2/main.go
@@ -8,25 +8,41 @@ import (
 
 func main() {
 	//FORMAT HHMMSS12h = "03:04:05PM  TO 24h OutputString := "15:04:05"
-	s := "06:56:45PM"
+	s := "12:10:05AM"
 	conversion := timeConversion(s)
 	fmt.Println(conversion)
 }
+
 func timeConversion(s string) string {
+	//convert to 3 slices 03:04:05PM
 	separateTime := strings.SplitAfter(s, ":")
-	//convert to 3 slices  the hour -->  06:56:45APM
 	hours := separateTime[0]
 	minutes := separateTime[1]
 	secondsFormat := separateTime[2]
-	//separate AM OR PM from seconds--> slice[2]
+	//separate AM OR PM from seconds[2]
 	seconds := secondsFormat[:2]
 	format := secondsFormat[2:]
+	/*fmt.Println(hours)
+	fmt.Println(minutes)
+	fmt.Println(secondsFormat)
+	fmt.Println(seconds)
+	fmt.Println(format)
+	*/
+
 	//Conversion string to int for hours and minutes
 	hoursConv, err := strconv.Atoi(hours[:2])
 	if err != nil {
 		fmt.Println("There is an error:", err)
 		return ""
 	}
+	/*minutesConv, err := strconv.Atoi(minutes[:2])
+	if err != nil {
+		fmt.Println("There's an error:", err)
+		return """
+	}
+	*/
+	//fmt.Println(hoursConv)
+	//fmt.Println(minutesConv)
 	//validate if format  it's AM OR PM
 	if format == "AM" && hoursConv == 12 {
 		hoursConv = 0
@@ -38,5 +54,6 @@ func timeConversion(s string) string {
 	*/
 	hours = fmt.Sprintf("%02d:", hoursConv)
 	//hours := strconv.Itoa(hoursConv)
+	//minutes = fmt.Sprintf("%02d", minutesConv)
 	return hours + minutes + seconds
 }
