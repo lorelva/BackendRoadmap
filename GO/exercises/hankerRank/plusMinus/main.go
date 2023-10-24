b80ac7a2799a634692ab75666f3d4b19ff781aff
lorelval <lore1268valle@gmail.com>
2023-10-24 14:18:52 -0600

    THinking on the solution

---
+++ GO/exercises/hankerRank/plusMinus/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	n := 6
+	arr := []int{-4, 3, -9, 0, 4, 1}
+
+	fmt.Println(n)
+	fmt.Println(arr)
+
+	//result := plusMinus()
+
+}
+func plusMinus(arr []int32) {
+
+}
