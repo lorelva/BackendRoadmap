5249b48c06258673815ebf071cd7e2a8bffb5bdd
lorelval <lore1268valle@gmail.com>
2023-11-04 17:58:47 -0600

    LSP example

---
+++ GO/designPatterns/examples_Solid_Principles/LSP/shapes/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+type Circle struct {
+	Radius float64
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi(c.Radius, c.Radius)
+}
+
+type Rectangle struct {
+	Width float64
+
+	Height float64
+}
+
+func (r *Rectangle) Area() float64 {
+
+	return r.Width * r.Height
+
+}
+
+func main() {
+
+}
