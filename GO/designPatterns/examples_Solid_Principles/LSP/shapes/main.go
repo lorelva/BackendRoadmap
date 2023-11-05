8c7ae3ce5628374eb8d9ce7f0810d9d65f08417b
lorelval <lore1268valle@gmail.com>
2023-11-04 20:47:50 -0600

    Changes

--- GO/designPatterns/examples_Solid_Principles/LSP/shapes/main.go
+++ GO/designPatterns/examples_Solid_Principles/LSP/shapes/main.go
@@ -15,14 +15,11 @@ func (c *Circle) Area() float64 {
 
 type Rectangle struct {
 	Width float64
-
 	Height float64
 }
 
 func (r *Rectangle) Area() float64 {
-
 	return r.Width * r.Height
-
 }
 
 func main() {
--- GO/fundamentals/interface/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/lorelva/BackendRoadmap/GO/fundamentals/interface
-
-go 1.21.1
