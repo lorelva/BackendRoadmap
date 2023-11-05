739e8c65419b4bb6b2426b981bc1cdc1e124b28d
lorelval <lore1268valle@gmail.com>
2023-11-04 20:47:00 -0600

    Second way to resolve time Converter, with help of someone with experience

--- GO/exercises/hankerRank/timeConverter2/main.go
+++ GO/exercises/hankerRank/timeConverter2/main.go
@@ -2,58 +2,24 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
+	"log"
+	"time"
 )
 
 func main() {
-	//FORMAT HHMMSS12h = "03:04:05PM  TO 24h OutputString := "15:04:05"
-	s := "12:10:05AM"
-	conversion := timeConversion(s)
-	fmt.Println(conversion)
-}
 
-func timeConversion(s string) string {
-	//convert to 3 slices 03:04:05PM
-	separateTime := strings.SplitAfter(s, ":")
-	hours := separateTime[0]
-	minutes := separateTime[1]
-	secondsFormat := separateTime[2]
-	//separate AM OR PM from seconds[2]
-	seconds := secondsFormat[:2]
-	format := secondsFormat[2:]
-	/*fmt.Println(hours)
-	fmt.Println(minutes)
-	fmt.Println(secondsFormat)
-	fmt.Println(seconds)
-	fmt.Println(format)
-	*/
+	h := "06:05:03AM"
 
-	//Conversion string to int for hours and minutes
-	hoursConv, err := strconv.Atoi(hours[:2])
+	// Layout de la hora/for the hour
+	//predefined layouts for use in Time(view in documentation official of GO)
+	t, err := time.Parse("03:04:05PM", h)
 	if err != nil {
-		fmt.Println("There is an error:", err)
-		return ""
+		log.Fatal(err)
 	}
-	/*minutesConv, err := strconv.Atoi(minutes[:2])
-	if err != nil {
-		fmt.Println("There's an error:", err)
-		return """
-	}
-	*/
-	//fmt.Println(hoursConv)
-	//fmt.Println(minutesConv)
-	//validate if format  it's AM OR PM
-	if format == "AM" && hoursConv == 12 {
-		hoursConv = 0
-	} else if format == "PM" && hoursConv != 12 {
-		hoursConv += 12
-	}
-	/*hoursReturn := strconv.Itoa(hoursConv)
-	minutesReturn := strconv.Itoa(minutesConv)
-	*/
-	hours = fmt.Sprintf("%02d:", hoursConv)
-	//hours := strconv.Itoa(hoursConv)
-	//minutes = fmt.Sprintf("%02d", minutesConv)
-	return hours + minutes + seconds
+
+	//Output format (how you need it)
+	// t.Format retorna el tiempo del tipo de dato t en el formato de la hora deseada
+	horaFormateada := t.Format("15:04:05")
+
+	fmt.Println(horaFormateada)
 }
