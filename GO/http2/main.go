689ebd74ed12ec7ffe4ea5d21ffb0e06e2e5a019
lorelval <lore1268valle@gmail.com>
2024-03-14 15:57:33 -0600

    Defining a route handler for the /nombre endpoint using the http.HandleFunc

--- GO/http2/main.go
+++ GO/http2/main.go
@@ -9,12 +9,25 @@ import (
 	"strconv"
 )
 
+// Para el endpoint de "/resta",request y response
 type requestJson struct {
 	Primero int `json:"primero,omitempty"`
 	Segundo int `json:"segundo,omitempty"`
 }
 
 type responseJson struct {
+	Message string `json:"message,omitempty"`
+	Status  int    `json:"status,omitempty"`
+}
+
+// Para el endpoint de "/name", request y repsonse
+type requestNameJson struct {
+	Nombre   string `json:"nombre,omitempty"`
+	Apellido string `json:"apellido,omitempty"`
+	Edad     int    `json:"edad,omitempty"`
+}
+
+type responseNameJson struct {
 	Message string `json:"message,omitempty"`
 	Status  int    `json:"status,omitempty"`
 }
@@ -70,7 +83,9 @@ func main() {
 
 	})
 
+	//Uso de Marshal y Unmarshal, para formato JSON en endpoint "/resta"
 	http.HandleFunc("/resta", func(writer http.ResponseWriter, request *http.Request) {
+		//Verificación del método HTTP deseado
 		if request.Method != "POST" {
 			writer.WriteHeader(http.StatusBadRequest)
 			return
@@ -78,6 +93,8 @@ func main() {
 
 		resta := requestJson{}
 
+		//Lectura del body del request
+		//body es de tipo io.ReadAll, y al final cerrar el body del request con defer
 		body, err := io.ReadAll(request.Body)
 		if err != nil {
 			writer.WriteHeader(http.StatusInternalServerError)
@@ -85,8 +102,11 @@ func main() {
 		}
 		defer request.Body.Close()
 
+		//json.Unmarshal()= recibir JSON para convertir en una estructura de datos Go.
 		err = json.Unmarshal(body, &resta)
 		if err != nil {
+			//happy Path--> hacer que falle
+			//responder como API
 			resJson, err := json.Marshal(responseJson{
 				Message: fmt.Sprintf("Hubo un error en Unmarshal, el error es %v", err),
 				Status:  http.StatusInternalServerError,
@@ -110,6 +130,7 @@ func main() {
 			Status:  http.StatusOK,
 		}
 
+		//json.Marshal()= Estructura de datos Go para convertir en JSON.
 		resJson, err := json.Marshal(res)
 		if err != nil {
 			writer.WriteHeader(http.StatusInternalServerError)
@@ -118,6 +139,69 @@ func main() {
 
 		writer.Header().Set("Content-Type", "application/json")
 		writer.Write(resJson)
+		//Metódo Write del http.ResponseWriter , escribe en el body del response(JSON)-->
+		//Para enviar un mensaje de respuesta al cliente en el cuerpo de la respuesta HTTP
+		//Ya convertido(casteo) el string(mensaje) a []bytes = cadena/arreglo de bytes
+		//writer.Write([]byte(send))
+		writer.WriteHeader(http.StatusAccepted)
+	})
+
+	http.HandleFunc("/nombre", func(writer http.ResponseWriter, request *http.Request) {
+		if request.Method != "POST" {
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		//Recibir JSON = Unmarshal
+		saludo := requestNameJson{}
+
+		//Leer el body del request
+		body, err := io.ReadAll(request.Body)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		//Cerrar el body del request
+		defer request.Body.Close()
+
+		//1er parámetro = bytes que contiene datos en formato JSON
+		//2do parámetro = puntero en formato GO = struct donde recibe el JSON
+		err = json.Unmarshal(body, &saludo)
+		if err != nil {
+			resJson, err := json.Marshal(responseJson{
+				Message: fmt.Sprintf("Error en Unmarshal, el error es %v", err),
+				Status:  http.StatusInternalServerError,
+			})
+			if err != nil {
+				writer.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			writer.Header().Set("Content-Type", "application/json")
+			writer.Write(resJson)
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		//Unir las variables para el mensaje final
+		msg := fmt.Sprintf("Hola %s %s, tienes %d años.¡Bienvenido!", saludo.Nombre, saludo.Apellido, saludo.Edad)
+		//writer.Write([]byte(msg))
+		//writer.WriteHeader(http.StatusAccepted)
+
+		//Responder JSON = Marshal
+		responseJson := responseNameJson{
+			Message: msg,
+			Status:  http.StatusOK,
+		}
+
+		res, err := json.Marshal(responseJson)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		writer.Header().Set("Content-Type", "application/json")
+		writer.Write(res)
 		writer.WriteHeader(http.StatusAccepted)
 	})
 
